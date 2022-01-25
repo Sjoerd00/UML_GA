1d5377577fe6e4075abd0243b9c0a6bc5cd38fa4
SjoerdStevens <stevens.sc@outlook.com>
2022-01-25 16:13:12 +0100

    Update Results.R

--- Code/Results.R
+++ Code/Results.R
@@ -10,15 +10,27 @@ X <- select(df, -c(name, club, Position, eur_value, eur_wage, eur_release_clause
 
 
 # PCA
-PCAobj <- prcomp(X, rank=5)
-PCAbipl(X)
+PCAobj <- prcomp(X)
 summary(PCAobj)
+plot(PCAobj, type='l')
+
+# PCA on Correlation Martrix
+Xcorr <- cor(X)
+PCAobj2 <- prcomp(Xcorr, rank)
+summary(PCAobj2)
+plot(PCAobj2, type='l')
+  
+# Rotation
+rotmat  <- varimax(PCAobj$rotation[,1:5])$rotmat
+PCAvarmax <- PCAobj$rotation[,1:5] %*% rotmat
+
+rotmat2  <- varimax(PCAobj2$rotation[,1:2])$rotmat
+PCAvarmax2 <- PCAobj2$rotation[,1:2] %*% rotmat2
 
 # Sparse PCA
-varimax(X)
-biplot(X)
-
 spc <- SPC(X, sumabsv=sqrt(33), K=5)
 spc$v
 
 # Plots
+plot(PCAbipl(X[,1:5]))
+plot(PCAbipl(Xcorr[1:29,1:29]))
