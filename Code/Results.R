653f1f7f22533366b70a41706aa21ef3717e6182
SjoerdStevens <36222985+Sjoerd00@users.noreply.github.com>
2022-01-25 20:12:07 +0100

    Results

--- Code/Results.R
+++ Code/Results.R
@@ -2,38 +2,39 @@ library(pacman)
 p_load(tidyverse,
        fastDummies,
        PMA,
-       UBbipl)
+       UBbipl,
+       dplyr)
 load("~/GitHub/UML_GA/Code/FIFA2017_NL.RData")
 
 df <- fifa %>% fastDummies::dummy_columns('Position')
-X <- select(df, -c(name, club, Position, eur_value, eur_wage, eur_release_clause)) %>% scale()
-
+X <- dplyr::select(df, -c(name, club, Position, eur_value, eur_wage, eur_release_clause)) %>% scale
 
 # PCA
-PCAobj <- prcomp(X)
+PCAobj <- prcomp(X, scale. = FALSE)
 summary(PCAobj)
 plot(PCAobj, type='l')
 
-# PCA on Correlation Martrix
+# PCA on Correlation Martrix for kaiser rule
 Xcorr <- cor(X)
-PCAobj2 <- prcomp(Xcorr, rank)
+PCAobj2 <- prcomp(Xcorr)
 summary(PCAobj2)
 plot(PCAobj2, type='l')
   
 # Rotation
-rotmat  <- varimax(PCAobj$rotation[,1:5])$rotmat
-PCAvarmax <- PCAobj$rotation[,1:5] %*% rotmat
+rotmat  <- varimax(PCAobj$rotation[,1:2])$rotmat
+PCAvarmax <- PCAobj$rotation[,1:2] %*% rotmat
 
 rotmat2  <- varimax(PCAobj2$rotation[,1:2])$rotmat
 PCAvarmax2 <- PCAobj2$rotation[,1:2] %*% rotmat2
 
 # Sparse PCA
 # add optimal Caio values
-spc <- SPC(X, sumabsv=sqrt(33), K=5)
+spc <- SPC(X, sumabsv=sqrt(29), K=2)
 spc$v
 
 pmd <- PMD(X, sumabsu = 1, sumabsv = 1)
 pmd
+
 # Plots
-plot(PCAbipl(X[,1:5]))
+plot(PCAbipl(X))
 plot(PCAbipl(Xcorr[1:29,1:29]))
