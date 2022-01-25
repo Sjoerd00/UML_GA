8b100db0103cd4691e96747635fe78831e978284
SjoerdStevens <36222985+Sjoerd00@users.noreply.github.com>
2022-01-25 20:46:58 +0100

    Update Results.R

--- Code/Results.R
+++ Code/Results.R
@@ -3,20 +3,25 @@ p_load(tidyverse,
        fastDummies,
        PMA,
        UBbipl,
-       dplyr)
+       dplyr,
+       datawizard)
 load("~/GitHub/UML_GA/Code/FIFA2017_NL.RData")
 
+summary(fifa)
+
+fifa$clubNR <- as.numeric(fifa$club)
 df <- fifa %>% fastDummies::dummy_columns('Position')
+df <- fifa %>% datawizard::demean("crossing", group="club")
 X <- dplyr::select(df, -c(name, club, Position, eur_value, eur_wage, eur_release_clause)) %>% scale
 
 # PCA
-PCAobj <- prcomp(X, scale. = FALSE)
+PCAobj <- prcomp(X, scale. = FALSE, rank=2)
 summary(PCAobj)
 plot(PCAobj, type='l')
 
-# PCA on Correlation Martrix for kaiser rule
+# PCA on Correlation Matrix for kaiser rule
 Xcorr <- cor(X)
-PCAobj2 <- prcomp(Xcorr)
+PCAobj2 <- prcomp(Xcorr, 2)
 summary(PCAobj2)
 plot(PCAobj2, type='l')
   
@@ -29,7 +34,7 @@ PCAvarmax2 <- PCAobj2$rotation[,1:2] %*% rotmat2
 
 # Sparse PCA
 # add optimal Caio values
-spc <- SPC(X, sumabsv=sqrt(29), K=2)
+spc <- SPC(X, sumabsv=sqrt(dim(X)[2]), K=2)
 spc$v
 
 pmd <- PMD(X, sumabsu = 1, sumabsv = 1)
